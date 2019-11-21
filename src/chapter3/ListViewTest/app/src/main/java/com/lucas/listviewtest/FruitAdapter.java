b7cb114a766077d4353841b11cb2799551f0038d
lucasktian <lucasktian@tencent.com>
2019-11-21 20:02:08 +0800

    Update: finish 3.6 RecyclerView

--- src/chapter3/ListViewTest/app/src/main/java/com/lucas/listviewtest/FruitAdapter.java
+++ src/chapter3/ListViewTest/app/src/main/java/com/lucas/listviewtest/FruitAdapter.java
@@ -24,19 +24,56 @@ public class FruitAdapter extends ArrayAdapter<Fruit> {
         resourceId = textViewResourceId;
     }
 
+    /*
+     原始的方法
+     当每个子项被滚动到屏幕内的时候,getView 方法会被调用
+     */
+//    @NonNull
+//    @Override
+//    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+//        Fruit fruit = getItem(position);
+//
+//        View view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+//
+//        ImageView fruitImage = view.findViewById(R.id.fruit_image);
+//        TextView fruitName = view.findViewById(R.id.fruit_name);
+//        Log.d("Tag", "invoke getView:" + fruit.getName());
+//
+//        fruitImage.setImageResource(fruit.getImageId());
+//        fruitName.setText(fruit.getName());
+//        return view;
+//    }
+
+
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         Fruit fruit = getItem(position);
 
-        View view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+        View view;
 
-        ImageView fruitImage = view.findViewById(R.id.fruit_image);
-        TextView fruitName = view.findViewById(R.id.fruit_name);
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+
+            viewHolder = new ViewHolder();
+            viewHolder.fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+            viewHolder.fruitName = (TextView)view.findViewById(R.id.fruit_name);
+
+            view.setTag(viewHolder);    // 将 viewHolder 存储在 View 中
+
+        } else {
+            view = convertView;
+            viewHolder = (ViewHolder) view.getTag(); // 重新获取 ViewHolder
+        }
+
+        viewHolder.fruitImage.setImageResource(fruit.getImageId());
+        viewHolder.fruitName.setText(fruit.getName());
         Log.d("Tag", "invoke getView:" + fruit.getName());
-
-        fruitImage.setImageResource(fruit.getImageId());
-        fruitName.setText(fruit.getName());
         return view;
+    }
+    class ViewHolder {
+        ImageView fruitImage;
+        TextView fruitName;
     }
 }
